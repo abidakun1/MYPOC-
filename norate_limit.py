ed63b13e7804aa05e2368621d27d77758def6e7e
abidakun1 <83501065+abidakun1@users.noreply.github.com>
2022-11-15 10:57:12 -0800

    Create norate_limit.py

---
+++ norate_limit.py
@@ -0,0 +1,25 @@
+
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import json
+
+values = {'username': 'user', 'password': 'pass'}
+
+def get_data(url):
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+    response = requests.post(url, values, verify = False)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        return data
+    else:
+        print(response.text)
+        print(response.status_code)
+
+for i in range(5000):
+    base_url = "https://vsp-int.thefacebook.com/auriga/api/auth/token"
+    data = get_data(base_url)
+
+
+print("I have just sent 5000 request with no rate limiting in place,same status code && same response ")
