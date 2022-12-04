e7da4e4ee2df731d909747442b7bb337a7295bab
abidakun1 <83501065+abidakun1@users.noreply.github.com>
2022-12-04 05:19:28 -0800

    Update hstv.py

--- hstv.py
+++ hstv.py
@@ -8,7 +8,7 @@ G = "\033[0;32m"
 R = "\033[0;31m"
 
 if len(sys.argv) != 2:
-    print(f"Usage: python3 hstv.py example.com ")
+    print(f"Usage: python3 hstv.py https://example.com ")
     exit(1)
 
 url = sys.argv[1]
